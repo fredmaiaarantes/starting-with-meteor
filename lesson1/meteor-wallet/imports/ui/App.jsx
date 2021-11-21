3fc1a47242ff95319221fe333db5687c473acd4c
Frederico Maia Arantes <fredmaiaarantes@gmail.com>
2021-11-21 13:56:04 -0300

    Add Galaxy to the title

--- lesson1/meteor-wallet/imports/ui/App.jsx
+++ lesson1/meteor-wallet/imports/ui/App.jsx
@@ -4,7 +4,7 @@ import {ContactList} from "./ContactList";
 
 export const App = () => (
   <div>
-    <h1>Meteor Wallet</h1>
+    <h1>Meteor Wallet - Galaxy</h1>
     <ContactForm />
     <ContactList />
   </div>
